d7e508348689523ca06c53f48af44e7f98675bd1
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-08-04 22:03:07 -0300

    aplicação com express

---
+++ 06-express/index.js
@@ -0,0 +1,11 @@
+let express =  require('express');
+let app = express();
+
+
+app.get('/',function(req, res){
+    res.send('primeira rota com express');
+});
+
+app.listen(3000, function(){
+    console.log('aplicação rodando na porta 3000');
+})
