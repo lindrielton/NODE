186aecf7c8c1230b2605801e3028244691d6b6e6
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-08-06 06:16:16 -0300

    utilizando postman

--- 06-express/index.js
+++ 06-express/index.js
@@ -6,6 +6,13 @@ app.get('/',function(req, res){
     res.send('primeira rota com express');
 });
 
+app.get('/teste',function(req, res){
+    res.send('testando rota ');
+});
+
+
+
+
 app.listen(3000, function(){
     console.log('aplicação rodando na porta 3000');
 })
